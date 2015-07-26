7320f4f96c8507e00baf35d235c8e0e51820ae23
Deepak Kundu <dpk@brahmadpk.com>
2015-07-26 13:25:05 +0530

    remove admin from google analytics

--- app/mailers/podcast_mailer.rb
+++ app/mailers/podcast_mailer.rb
@@ -2,13 +2,13 @@ class PodcastMailer < ActionMailer::Base
   default from: "info@ipdb.net"
 
   def approval_email(podcast)
-  	@podcast = podcast
-  	mail(to: [['serena@ipdb.net'],['brooke@ipdb.net'],['josiah@ipdb.net']], subject: 'Podcast needs approval')
+    @podcast = podcast
+    mail(to: [['serena@ipdb.net'],['brooke@ipdb.net'],['josiah@ipdb.net']], subject: 'Podcast needs approval')
   end
 
   def processing_email(user, podcast)
-  	@user = user
-  	@podcast = podcast
-  	mail(to: @user.email, subject: 'Your Podcast is currently being proccesed')
+    @user = user
+    @podcast = podcast
+    mail(to: @user.email, subject: 'Your Podcast is currently being proccesed')
   end
 end
