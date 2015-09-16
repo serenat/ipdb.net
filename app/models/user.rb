9a225396556f6e2bec34805a2551bb3e9595be4b
bood-pvb <pvb80888@gmail.com>
2015-09-16 19:25:37 +0600

    SM links on profile page open a new tab

--- app/models/user.rb
+++ app/models/user.rb
@@ -5,7 +5,7 @@ class User < ActiveRecord::Base
     :recoverable, :rememberable, :trackable, :validatable, :database_authenticatable
   has_many :user_podcasts
   has_many :podcasts, :through => :user_podcasts
-  has_attached_file :profile_image, :default_url => "/images/ipdb.png", :styles => { :medium => "300x300>", :thumb => "100x100>" }
+  has_attached_file :profile_image, :default_url => "ipdb.png", :styles => { :medium => "300x300>", :thumb => "100x100>" }
   validates_attachment_content_type :profile_image, :content_type => %w(image/jpeg image/jpg image/png)
   letsrate_rater
   acts_as_commontator
