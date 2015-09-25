3c2a63f9b98ece6feb8cf1629d4e2a71e9a84c94
bood-pvb <pvb80888@gmail.com>
2015-09-26 02:38:19 +0600

    Stop slider on input focus

--- app/assets/javascripts/scripts.js
+++ app/assets/javascripts/scripts.js
@@ -1,15 +1,18 @@
 jQuery(document).ready(function($) { 
-	'use strict';
-  	$('.flexslider').flexslider({
-        animation: "slide", 
-        slideshow: true, 
-        slideshowSpeed: 10000,
-        animationSpeed: 500,
-        smoothHeight: true,
-        start: function(slider){
-          $('body').removeClass('loading'); 
-          $('.body-overlay').fadeToggle(); 
-        }
-  	});      
-});
+  'use strict';
+  $('.flexslider').flexslider({
+    animation: "slide",
+    slideshow: true,
+    slideshowSpeed: 10000,
+    animationSpeed: 500,
+    smoothHeight: true,
+    start: function(slider){
+      $('body').removeClass('loading');
+      $('.body-overlay').fadeToggle();
+    }
+  });
 
+  $('.flexslider input').on('focus', function(){
+    $('.flexslider').flexslider("stop");
+  });
+});
