b31a5c40a701494ac6b72e3a830a99d16529d164
bood-pvb <pvb80888@gmail.com>
2015-09-26 02:54:46 +0600

    Remove old codebase

--- old/config/unicorn.rb
+++
@@ -1,8 +0,0 @@
-# Unicorn config
-working_directory "/var/www/vhosts/ipdb.net"
-listen 8080 
-worker_processes 10
-pid "/var/www/vhosts/ipdb.net/tmp/pids/unicorn.pid"
-user "rails", "rails"
-stderr_path "/var/www/vhosts/ipdb.net/log/unicorn.log"
-stdout_path "/var/www/vhosts/ipdb.net/log/unicorn.log"
