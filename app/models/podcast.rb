ad704f84efbcea40d03b42ac02c72bc8d18a35f3
bood-pvb <pvb80888@gmail.com>
2015-11-02 16:20:40 +0600

    Change podcast img placeholder. Fix episode title.

--- app/models/podcast.rb
+++ app/models/podcast.rb
@@ -9,7 +9,7 @@ class Podcast < ActiveRecord::Base
   accepts_nested_attributes_for :nominations, allow_destroy: true
 
   has_attached_file :image, styles: {medium: "250x250", thumb: "100x100>"},
-    default_url: ->(attachment) { ActionController::Base.helpers.asset_path('microphone-slash.png') }
+    default_url: ->(attachment) { ActionController::Base.helpers.asset_path('microphone.png') }
 
   validates_attachment_content_type :image, :content_type => /\Aimage\/.*\Z/
   acts_as_commontable
